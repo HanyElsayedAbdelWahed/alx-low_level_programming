bc571ed82c1799b24495c428ba5f04e98e04acd4
Hany Elsayed <e.hany.elsayed@gmail.com>
2023-09-09 02:14:37 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A programe that print a ine using printf function
+ * Print the first line of the art with proper grammar
+ * Print the second line of the art with proper grammar
+ * return value of main Return 0 to indicate successful executionn
+ * return- 0 (success)
+ */
+int main(void)/*main function*/
+{
+printf("This is a piece of art,\n");
+printf("But the outcome is a masterpiece!\n");
+return (0);
+}
